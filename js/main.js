7ee74eaae672483f53da6ecda430429388f999a5
Zafar <zafar.a.ali2@gmail.com>
2019-12-05 12:48:16 -0500

    Fix scrolling too far issue with navlinks

--- js/main.js
+++ js/main.js
@@ -8,9 +8,9 @@ $(document).ready(function() {
       $(window).scroll(function() {
         let scroll = $(window).scrollTop();
         if (scroll >= nav_offset_top) {
-          $('.header_area .main-menu').addClass('navbar_fixed');
+          $('.header_area .main-menu').removeClass('navbar_fixed_hide');
         } else {
-          $('.header_area .main-menu').removeClass('navbar_fixed');
+          $('.header_area .main-menu').addClass('navbar_fixed_hide');
         }
       });
     }
@@ -22,7 +22,7 @@ $(document).ready(function() {
     var targetPosition = $(targetElement).offset().top;
     $('html, body').animate(
       {
-        scrollTop: targetPosition - 180
+        scrollTop: targetPosition - 75
       },
       'slow'
     );
@@ -33,21 +33,13 @@ $(document).ready(function() {
     var targetPosition = $(targetElement).offset().top;
 
     let scroll = $(window).scrollTop();
-    if (scroll >= nav_offset_top) {
-      $('html, body').animate(
-        {
-          scrollTop: targetPosition - 75
-        },
-        'fast'
-      );
-    } else {
-      $('html, body').animate(
-        {
-          scrollTop: targetPosition - 163
-        },
-        'fast'
-      );
-    }
+
+    $('html, body').animate(
+      {
+        scrollTop: targetPosition - 75
+      },
+      'fast'
+    );
   });
 
   $(document).on('click', '.nav-item a', function(e) {
@@ -79,41 +71,41 @@ $(document).ready(function() {
     }
   });
 
-  /*Scrolling highlight the active nav item */
-  $(window).scroll(function(event) {
-    var scrollHeight = $(document).height();
-    var scrollPosition = $(window).height() + $(window).scrollTop();
-    var scrollPos = $(document).scrollTop() + 75;
-    if ($(document).scrollTop() === 0) {
-      $('a[href^="#home"]')
-        .parent('li')
-        .addClass('active')
-        .siblings()
-        .removeClass('active');
-      return;
-    } else if (scrollPosition >= scrollHeight - 100) {
-      $('a[href^="#contact"]')
-        .parent('li')
-        .addClass('active')
-        .siblings()
-        .removeClass('active');
-      return;
-    }
+  // /*Scrolling highlight the active nav item */
+  // $(window).scroll(function(event) {
+  //   var scrollHeight = $(document).height();
+  //   var scrollPosition = $(window).height() + $(window).scrollTop();
+  //   var scrollPos = $(document).scrollTop() + 75;
+  //   if ($(document).scrollTop() === 0) {
+  //     $('a[href^="#home"]')
+  //       .parent('li')
+  //       .addClass('active')
+  //       .siblings()
+  //       .removeClass('active');
+  //     return;
+  //   } else if (scrollPosition >= scrollHeight - 100) {
+  //     $('a[href^="#contact"]')
+  //       .parent('li')
+  //       .addClass('active')
+  //       .siblings()
+  //       .removeClass('active');
+  //     return;
+  //   }
 
-    $('.nav-item a').each(function() {
-      var currLink = $(this);
-      var refElement = $(currLink.attr('href'));
-      if (
-        refElement.position().top <= scrollPos &&
-        refElement.position().top + refElement.height() > scrollPos
-      ) {
-        $('.nav-item').removeClass('active');
-        currLink.parent().addClass('active');
-      } else {
-        currLink.parent().removeClass('active');
-      }
-    });
-  });
+  //   $('.nav-item a').each(function() {
+  //     var currLink = $(this);
+  //     var refElement = $(currLink.attr('href'));
+  //     if (
+  //       refElement.position().top <= scrollPos &&
+  //       refElement.position().top + refElement.height() > scrollPos
+  //     ) {
+  //       $('.nav-item').removeClass('active');
+  //       currLink.parent().addClass('active');
+  //     } else {
+  //       currLink.parent().removeClass('active');
+  //     }
+  //   });
+  // });
 
   /**Collapse Nav on mobile when clicking link */
   $('.navbar-collapse a').click(function() {
